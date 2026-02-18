8b1790031971e40c8fe68aebc729cb1ab5fe8e06
Simon Charest <simoncharest@gmail.com>
2026-02-18 16:37:27 -0500

    Adding functions to read video stream

--- __main__.py
+++ __main__.py
@@ -1,10 +1,14 @@
 from argparse import ArgumentParser, Namespace
+from cv2 import VideoCapture, imshow, waitKey, destroyAllWindows
+from cv2.typing import MatLike
+import ffmpeg
 from glob import glob
 from io import TextIOWrapper
 from pandas import DataFrame, json_normalize, set_option
 from pathlib import Path
 from sqlite3 import Connection
 from typing import Any
+from urllib.parse import quote
 
 from src.explode import explode
 from src.get_alarm_data import get_alarm_data
@@ -17,8 +21,6 @@ def main() -> None:
     arguments: Namespace = parse_arguments()
     configuration: dict[str, Any] = load_configuration(Path(__file__).parent.joinpath("config.json"), arguments.verbose)
     connection: Connection = get_connection(configuration["database"], arguments.verbose)
-    path: str
-    data: DataFrame
 
     set_option("display.max_rows", None)
     set_option("display.max_columns", None)
@@ -26,22 +28,39 @@ def main() -> None:
 
     if arguments.get_alarm_data is not None:
         if len(arguments.get_alarm_data):
-            path = arguments.get_data
+            path: str = arguments.get_data
 
         else:
-            path = configuration["alarm"]["data"]
+            path: str = configuration["alarm"]["data"]
         
-        data = get_alarm_data(path, arguments.verbose)
+        data: DataFrame = get_alarm_data(path, arguments.verbose)
         write(data, connection, "alarms", "replace", False, arguments.verbose)
+
+    if arguments.get_camera_data:
+        filename: str = f"{configuration["camera"]["protocol"]}://{quote(configuration["camera"]["username"])}:{quote(configuration["camera"]["password"])}@{configuration["camera"]["ip"]}:{configuration["camera"]["port"]}/unicast/c{configuration["camera"]["channel"]}/s{configuration["camera"]["stream"]}/live"
+        video_capture: VideoCapture = VideoCapture(filename)
+
+        if not video_capture.isOpened():
+            raise Exception("Failed to open stream.")
+
+        return_value: bool
+        frame: MatLike
+        return_value, frame = video_capture.read()
+        video_capture.release()
+
+        if not return_value:
+            raise Exception("Failed to read frame.")
+        
+        print(frame)
 
     if arguments.get_token_data is not None:
         if len(arguments.get_token_data):
-            path = arguments.get_data
+            path: str = arguments.get_data
 
         else:
-            path = configuration["token"]["data"]
+            path: str = configuration["token"]["data"]
 
-        data = get_token_data(path, arguments.verbose)
+        data: DataFrame = get_token_data(path, arguments.verbose)
         write(data, connection, "tokens", "replace", False, arguments.verbose)
 
     if arguments.get_user_data is not None:
@@ -57,6 +76,7 @@ def main() -> None:
 
     if arguments.list:
         paths: list[str] = glob("sql/**/*.sql", recursive=True)
+        path: str
        
         for path in paths:  
                 print(Path(path).as_posix())
@@ -65,15 +85,16 @@ def main() -> None:
         stream: TextIOWrapper = open(Path(__file__).parent.joinpath(arguments.query))
         sql: str = stream.read()
         stream.close()
-        data = query(sql, connection, arguments.verbose)
+        data: DataFrame = query(sql, connection, arguments.verbose)
         print(data)
 
     disconnect(connection, arguments.verbose)
 
 
 def parse_arguments() -> Namespace:
-    argument_parser: ArgumentParser = ArgumentParser(description="PyCentral: Python Kantech Alarm and Token Log Analyser")
+    argument_parser: ArgumentParser = ArgumentParser(description="PyCentral: Data Acquisition & Analytics")
     argument_parser.add_argument("-A", "--get_alarm_data", nargs="?", const="", type=str, help="Get alarm data")
+    argument_parser.add_argument("-C", "--get_camera_data", action="store_true", help="Get camera data")
     argument_parser.add_argument("-T", "--get_token_data", nargs="?", const="", type=str, help="Get token data")
     argument_parser.add_argument("-U", "--get_user_data", nargs="?", const="", type=str, help="Get user data")
     argument_parser.add_argument("-l", "--list", action="store_true", help="List available queries")
